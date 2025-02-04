b4b9c88dc6f14fdbc0aecb2f53251dd96c133230
JFarah <joaquinefarah@gmail.com>
2025-02-04 10:42:39 -0300

    feat: add Carousel component with navigation; implement Card component for carousel items; update package dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,42 @@
 import React from "react";
 import Header from "../components/Header";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 export default function Home() {
   return (
     
-    <div>
+    <div className=" px-20">
       <Header/>
       <h1>MANAGE LAB</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
       </p>
       <Button>Click me</Button>
+      <Carousel className="w-full max-w-xs ml-20">
+      <CarouselContent>
+        {Array.from({ length: 3 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
     </div>
   );
 }
