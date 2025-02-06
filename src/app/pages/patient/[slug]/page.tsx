5c6a833784946abf2b744cf45170f4c7b19c29f4
JFarah <joaquinefarah@gmail.com>
2025-02-06 12:18:08 -0300

    feat: update patient data structure to include phone numbers and modify email display to phone

--- src/app/pages/patient/[slug]/page.tsx
+++ src/app/pages/patient/[slug]/page.tsx
@@ -2,11 +2,11 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const patients = [
-  { id: "11827330", name: "Juan Pérez", age: 34, slug: "11827330", email: "juan.perez@example.com", analyses: [] },
-  { id: "22345675", name: "María López", age: 29, slug: "22345675", email: "maria.lopez@example.com", analyses: [] },
-  { id: "31334887", name: "Carlos Gómez", age: 41, slug: "31334887", email: "carlos.gomez@example.com", analyses: [] },
-  { id: "45984275", name: "Ana Martínez", age: 36, slug: "45984275", email: "ana.martinez@example.com", analyses: [] },
-  { id: "95000543", name: "Pedro Sánchez", age: 50, slug: "95000543", email: "pedro.sanchez@example.com", analyses: [] },
+  { id: "11827330", name: "Juan Pérez", age: 34, slug: "11827330", cel:"26111827330", analyses: [] },
+  { id: "22345675", name: "María López", age: 29, slug: "22345675", cel:"26122345675", analyses: [] },
+  { id: "31334887", name: "Carlos Gómez", age: 41, slug: "31334887", cel:"26131334887", analyses: [] },
+  { id: "45984275", name: "Ana Martínez", age: 36, slug: "45984275", cel:"26145984275", analyses: [] },
+  { id: "95000543", name: "Pedro Sánchez", age: 50, slug: "95000543", cel:"26195000543", analyses: [] },
 ];
 
 // Genera rutas estáticas
@@ -35,7 +35,7 @@ const PatientProfile = ({ params }: { params: { slug: string } }) => {
           <strong>Edad:</strong> {patient.age}
         </p>
         <p>
-          <strong>Email:</strong> {patient.email}
+          <strong>Celular:</strong> {patient.cel}
         </p>
         <h2 className="text-xl mt-4">Historial de Análisis:</h2>
         {patient.analyses.length === 0 ? (
